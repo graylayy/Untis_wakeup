108ca56b5a303ebd38f29d42b84ce98b9422a283
Jan Schneider <jan@schneider-mai.net>
2020-11-21 01:19:50 +0100

    developed bot further

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const TeleBot =require("telebot");
 const config=require("./config");
+const axios = require('axios');
 
 const bot = new TeleBot({
     token: config.telebot_apikey,
@@ -51,12 +52,13 @@ bot.on('ask.isMAllincrodt', (msg) => {
 bot.on('ask.firmware', (msg) => {
     const id = msg.chat.id;
     var selection = msg.text;
+    
+    //-----Android----
     if(selection=="android"){
        
         let replyMarkup = bot.inlineKeyboard([
             [bot.inlineButton('MacroDroid', {url: 'https://play.google.com/store/apps/details?id=com.arlosoft.macrodroid'})]
         ])
-        
         
         bot.sendMessage(id, "Die App \"MacroDroid\" kann das ereichen, was wir wollen! Bitte öffne nach dem Installieren der App die Datei, welche ich dir gesensedet habe (Alarm_wakeup.macro)",{replyMarkup: 'hide'})
         .then(()=>{
@@ -67,21 +69,72 @@ bot.on('ask.firmware', (msg) => {
                 bot.sendMessage(id, "Schicke mir einfach den Webhook (die URL aus dem roten Berreich), wenn du fertig bist!",{ask: 'webhook'});
             },700,id)
         })
-            
-    
-    
+    //----IOS----
     }else if (selection==="ios"){
-        bot.sendMessage(id, "Dann musst du selbst herausfinden, wie ich dir einen Wecker stelllen kann!");
+        bot.sendMessage(id, "Es ist leider aktuell für mich nicht möglch automatisiert einen Wecker für dich zu stellen!",{replyMarkup: 'hide'});
+        setTimeout((id) => {
+            bot.sendMessage(id, "Du kannst mir aber trotzdem jetzt eine Nachicht mit einem Webhook senden, damit ich ihn zu deiner Weckzeit auslösen kann!",{ask: 'webhook'});
+        },1000,id)
+    //----weiteres----
     }else if(selection==="was anderes"){
-        bot.sendMessage(id, "Dann musst du selbst herausfinden, wie ich dir einen Wecker stelllen kann!");
+        bot.sendMessage(id, "Dann musst du selbst herausfinden, wie ich dir einen Wecker stelllen kann!",{replyMarkup: 'hide'});
+        setTimeout((id) => {
+            bot.sendMessage(id, "Schick mir einfach den Webhook, welchen ich zu deiner Weckzeit aktivieren soll, falls du einen Weg herausgefunden hast!",{ask: 'webhook'});
+        },1000,id)
     }else{
         bot.sendMessage(id, "😕 Bitte benutze du unteren Schaltflächen um zu antworten!",{ask: 'firmware'});
     }
 });
 
+bot.on('ask.webhook', (msg) => {
+    const id = msg.chat.id;
+    bot.sendMessage(id, "Ich werden Webhook jetzt testen!");
+    setTimeout((id) => {
+        bot.sendMessage(id, "Bitte schau, ob der Webhook die gewünschte Aktion ausgeführt hat!");
+        
+        try{
+        axios.get(msg.text)
+        }catch(e){console.log(e);}
+        
+        //
+        //text in SQL database einfügen!
+        //
+        
+        let replyMarkup = bot.keyboard([
+            [bot.button('ja', 'ja'), bot.button('nein', 'nein')]
+        ], {resize: true});
+
+        setTimeout((id) => {
+            bot.sendMessage(id, "Ist die Aktion ausgeführt worden?",{replyMarkup,ask: 'webhookcorrect'});
+        },2500,id)
+
+    },1000,id)
+});
+
+bot.on('ask.webhookcorrect', (msg) => {
+    const id = msg.chat.id;
+    var selection = msg.text;
+
+    if(selection==="ja"){
+        bot.sendMessage(id, "Dann sind wir schon fast fertig! 🥳",{replyMarkup: 'hide'});
+
+    }else if(selection==="nein"){
+        bot.sendMessage(id, "😥 Dann ist wohl etwas schiefgelaufen! schua nochmal nach, ob der Webhook richtg war un sende ihn mir danach nochmals zu!",{ask: 'webhook',replyMarkup: 'hide'});
+    } else{
+        bot.sendMessage(id, "😕 Bitte benutze du unteren Schaltflächen um zu antworten!",{ask: 'webhookcorrect'});
+    }
+
+});
+
+//Fächerauswahl
+
+//Aktivieren/deaktivieren
+
+
+/*
 async function main(msg){
     const get = await bot.getMe()
     console.log(msg);
 }
-
+*/
 bot.start();
