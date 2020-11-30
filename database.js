fc5e168fbd53600633fb193f791dbd8262012707
Jan Schneider <jan@schneider-mai.net>
2020-11-30 21:34:09 +0100

    database now updates

--- database.js
+++ database.js
@@ -12,7 +12,14 @@ module.exports.newUser =async function newUser(attributes){
     let conn;
     try {
         conn = await pool.getConnection();
+        let isindb =await conn.query("SELECT * FROM untis.wecker_users WHERE chatID=?",attributes[0])
+        console.log(isindb);
+        if(isindb[0]==undefined){
         await conn.query("INSERT INTO untis.wecker_users (chatID,name,webhook1,enabeled) VALUES (?,?,?,?);",attributes);
+        } else{
+            attributes.push(attributes[0])
+            await conn.query("UPDATE untis.wecker_users SET chatID=?,name=?,webhook1=?,enabeled=? WHERE chatID=?;",attributes)
+        }
     } catch (err) {
 	    throw err;
     } finally {
